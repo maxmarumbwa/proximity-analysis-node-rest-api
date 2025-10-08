017ff2cc5e314fed877ef5e4f53954fede67ceeb
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-08 18:22:20 +0200

    data model & route to post to db via postman

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,26 @@
 const express = require ('express');
 const router = express.Router();
+const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req, res){
+router.get('/ninjas', function(req, res, next){
     res.send({type: 'GET'});
 });
 
 // add a new ninja to the db
-router.post('/ninjas', function(req, res){
-    console.log('You made a POST request: ', req.body);
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        rank: req.body.rank
-    });
+router.post('/ninjas', function(req, res, next){
+    Ninja.create(req.body).then(function(ninja){
+        res.send(ninja);
+    }).catch(next);
 });
 
 // update a ninja in the db
-router.put('/ninjas/:id', function(req, res){
+router.put('/ninjas/:id', function(req, res, next){
     res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res){
+router.delete('/ninjas/:id', function(req, res, next){
     res.send({type: 'DELETE'});
 });
 
